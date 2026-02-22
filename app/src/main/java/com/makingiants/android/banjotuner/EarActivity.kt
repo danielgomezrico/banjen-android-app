9378f132b163b00cf8fedd713d6c91da9edd1e01
Dan <danielgomezrico@gmail.com>
2026-02-22 12:43:28 -0500

    style: sort testTag import alphabetically before res.*

--- app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
@@ -83,9 +83,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
